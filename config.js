924d67f3fb75e0434c06e729876b0d662b9e2e99
Hugo Menezes <hugomenezes@gmail.com>
2017-12-28 15:42:49 -0300

    variavel para o docker-compose

--- config.js
+++ config.js
@@ -1,7 +1,9 @@
 var config = {};
 
+const executorHost = process.EXECUTOR_HOST || "localhost";
+
 config.PORT = 8081;
-config.executorUrl = "http://localhost:8085/executor";
+config.executorUrl = "http://" + executorHost + ":8085/executor";
 config.proxyPresentationUrl = "http://localhost:8086/event";
 //config.executorUrl = "http://localhost:8081/testexecutor";
 
