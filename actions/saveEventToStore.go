ca9dedf11c9e25b6dedf1ef0847a0db4407d18f5
Philippe Moneda <philippemoneda@gmail.com>
2018-04-02 19:14:54 -0300

    remove linha com erro

--- actions/saveEventToStore.go
+++ actions/saveEventToStore.go
@@ -1,15 +1,12 @@
 package actions
 
-
-package actions
-
 import (
 	"github.com/ONSBR/Plataforma-EventManager/domain"
 	"github.com/ONSBR/Plataforma-EventManager/eventstore"
-	"github.com/ONSBR/Plataforma-EventManager/executor"
 	log "github.com/sirupsen/logrus"
 )
 
+//SaveEventToStore send event to event store without dispatch to executor
 func SaveEventToStore(event domain.Event) error {
 	if event.Owner == "" {
 		event.Owner = "anonymous"
