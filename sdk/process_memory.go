4661d29b56aab2180fd29e257598cb87cb05ed66
Philippe Moneda <philippemoneda@gmail.com>
2018-07-24 10:25:37 -0300

    adiciona serviço para buscar todas as memorias

--- sdk/process_memory.go
+++ sdk/process_memory.go
@@ -45,3 +45,13 @@ func ReplaceDocument(collection string, query map[string]string, document interf
 	_, err := http.Put(url, document)
 	return err
 }
+
+type Memory map[string]interface{}
+
+//GetMemoryHistory returns all commit on process memory by instance
+func GetMemoryHistory(instanceID string) ([]Memory, error) {
+	url := fmt.Sprintf("%s%s/history?app_origin=event_manager", getProcessMemoryUrl(), instanceID)
+	result := make([]Memory, 0)
+	err := http.GetJSON(url, &result)
+	return result, err
+}
