18a5b1e4a914e9ad2042d8a879ed828995aa8daf
Philippe Moneda <philippemoneda@gmail.com>
2018-12-28 09:38:44 -0200

    adicionar logs

--- api/commands.go
+++ api/commands.go
@@ -1,6 +1,10 @@
 package api
 
 import (
+	"bytes"
+	"io/ioutil"
+	"time"
+
 	"github.com/ONSBR/Plataforma-EventManager/domain"
 	"github.com/ONSBR/Plataforma-EventManager/flow"
 	"github.com/gin-gonic/gin"
@@ -10,16 +14,27 @@ import (
 func registerCommandsAPI(r *gin.Engine) {
 	fullEventFlow := flow.GetEventRouter()
 	storeEventFlow := flow.GetBasicEventRouter()
+	r.Use(func(c *gin.Context) {
+		var bodyBytes []byte
+		if c.Request.Body != nil {
+			bodyBytes, _ = ioutil.ReadAll(c.Request.Body)
+		}
+		// Restore the io.ReadCloser to its original state
+		c.Request.Body = ioutil.NopCloser(bytes.NewBuffer(bodyBytes))
+		log.Info(string(bodyBytes))
+		c.Next()
+	})
 	r.PUT("/sendevent", func(c *gin.Context) {
 		event := domain.NewEvent()
 		var err error
 		if err = c.BindJSON(event); err == nil {
-			//begin := time.Now()
+
+			begin := time.Now()
 			err = fullEventFlow.Push(event)
 			if err != nil {
 				log.Error(err)
 			}
-			//log.Info("Tempo total:", time.Now().Sub(begin))
+			log.Info("Tempo total de processamento do evento:", time.Now().Sub(begin))
 			if err == nil {
 				c.JSON(200, gin.H{
 					"message": "OK",
@@ -27,11 +42,12 @@ func registerCommandsAPI(r *gin.Engine) {
 				return
 			}
 		}
-		c.JSON(200, gin.H{
+		log.Error(err.Error())
+		c.JSON(400, gin.H{
 			"message": err.Error(),
 		})
 	})
-	log.Info("Register route POST /save")
+	log.Debug("Register route POST /save")
 	r.POST("/save", func(c *gin.Context) {
 		event := new(domain.Event)
 		if err := c.BindJSON(event); err != nil {
@@ -39,11 +55,14 @@ func registerCommandsAPI(r *gin.Engine) {
 				"message": err.Error(),
 			})
 		}
+
+		begin := time.Now()
 		if err := storeEventFlow.Push(event); err != nil {
 			log.Error(err.Error())
 			c.JSON(200, err)
 		} else {
 			c.JSON(200, event)
 		}
+		log.Info("Tempo total de processamento do evento:", time.Now().Sub(begin))
 	})
 }
